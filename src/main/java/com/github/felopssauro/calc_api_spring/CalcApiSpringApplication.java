9a0a656b4fedbc086532e4400f51c2ec96d60ab4
Felopssauro <felops@protonmail.com>
2026-05-26 09:21:07 -0300

    Initial commit

---
+++ src/main/java/com/github/felopssauro/calc_api_spring/CalcApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.felopssauro.calc_api_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/felopssauro/calc_api_spring/CalcApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.felopssauro.calc_api_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
